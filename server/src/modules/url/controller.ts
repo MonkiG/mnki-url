b32531cfb57705cf282e6e56fc49e8980e5350b4
MonkiG <raan.heam@gmail.com>
2024-06-15 19:38:35 -0600

    feat: handle ShortUrl controller errors

--- server/src/modules/url/controller.ts
+++ server/src/modules/url/controller.ts
@@ -1,15 +1,23 @@
 import { type Request, type Response } from 'express'
 import * as ShorUrlServices from './services'
-import { RequestShorURL } from './models'
+import { ShortUrlRequest } from './models'
+import { HttpResponsesStatuses } from '../../core/enums/Responses'
 
 class ShortUrl {
   services = ShorUrlServices
   async create (req: Request, res: Response): Promise<void> {
     const { body } = req
-    const requestShortURl = new RequestShorURL(body)
-    const urlResponse = await this.services.createShortUrl(requestShortURl.url, requestShortURl.alias)
-
-    res.status(201).json(urlResponse)
+    try {
+      const requestShortURl = new ShortUrlRequest(body)
+      const urlResponse = await this.services.createShortUrl(requestShortURl)
+      res.status(201).json(urlResponse)
+    } catch (e: any) {
+      if (e.name === 'InvalidUrl') {
+        res.status(HttpResponsesStatuses.BAD_REQUEST).json({ message: 'Invalid url format' })
+        return
+      }
+      res.status(HttpResponsesStatuses.ERROR).json({ message: 'Server error' })
+    }
   }
 }
 
