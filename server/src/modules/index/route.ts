99ce4c2611e9e99f7d099aa8d164c504aeec2820
MonkiG <raan.heam@gmail.com>
2024-06-15 18:11:48 -0600

    feat: handle url error

--- server/src/modules/index/route.ts
+++ server/src/modules/index/route.ts
@@ -4,9 +4,6 @@ import { getOriginalUrl } from './services'
 export default class MainRouter extends BaseRouter {
   constructor () {
     super()
-    // this.router.get('/', (_, res) => {
-    //   res.json({ message: 'Hello, world!' })
-    // })
     this.router.get('/hello-world', (_, res) => {
       res.json({ message: 'Hello, world!' })
     })
@@ -14,8 +11,14 @@ export default class MainRouter extends BaseRouter {
       const { params } = req
       const { identifier } = params
 
-      const originalUrl = await getOriginalUrl(identifier)
-      res.redirect(originalUrl.href)
+      try {
+        const originalUrl = await getOriginalUrl(identifier)
+        res.redirect(originalUrl.href)
+      } catch (e: any) {
+        if (e.name === 'urlError') {
+          res.status(204).json({ message: 'Url don\'t found' })
+        }
+      }
     })
     this.defaultRoutes()
   }
--- server/src/modules/index/services.ts
+++ server/src/modules/index/services.ts
@@ -3,6 +3,10 @@ import { pool } from '../../context/context.postgres'
 export async function getOriginalUrl (identifier: string): Promise<URL> {
   const { rows: [selectedUrl] } = await pool.query('SELECT * FROM urls WHERE hash=$1 OR alias=$1', [identifier])
 
-  if (!selectedUrl) throw new Error('No url with the identifier found')
+  if (!selectedUrl) {
+    const urlError = new Error('No url with the identifier found')
+    urlError.name = 'urlError'
+    throw urlError
+  }
   return new URL(selectedUrl.original as string)
 }
